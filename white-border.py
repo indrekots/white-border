47300adea1097f58fc0e40049c855a95b3647aad
Indrek Ots <ots.indrek@gmail.com>
2018-05-29 22:44:19 +0300

    Add border to images in the same folder
    For every jpeg image in the current folder, add white border to it.
    The script will look for 'jpg' or 'jpeg' files, add a border to it
    and save the result to 'whiteBorder' folder.

--- white-border.py
+++ white-border.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python
 from PIL import Image
-import sys
+import sys, os
 
 borderRatio = 11.52
+whiteBorderFolder = 'whiteBorder'
 
-image = Image.open(sys.argv[1]);
-print image.size
+def isJpeg(fileName):
+    return '.jpg' in fileName or '.jpeg' in fileName
 
-newSize = (image.size[0] + int(round(image.size[0] / borderRatio)),
+def addWhiteBorder(fileName):
+    image = Image.open(fileName);
+
+    newSize = (image.size[0] + int(round(image.size[0] / borderRatio)),
     image.size[1] + int(round(image.size[1] / borderRatio)))
-print newSize
 
-newImage = Image.new("RGB", newSize, (255, 255, 255))
-newImage.paste(image, ((newImage.size[0] - image.size[0]) / 2, (newImage.size[1] - image.size[1]) / 2))
+    print 'Adding a white border to ' + fileName
+    newImage = Image.new("RGB", newSize, (255, 255, 255))
+    newImage.paste(image, ((newImage.size[0] - image.size[0]) / 2, (newImage.size[1] - image.size[1]) / 2))
+
+    if not os.path.exists(whiteBorderFolder):
+        os.makedirs(whiteBorderFolder)
+
+    newImage.save(whiteBorderFolder + '/' + fileName)
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    if isJpeg(f):
+        addWhiteBorder(f)
