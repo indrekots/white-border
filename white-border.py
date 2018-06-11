f2eea8b00710b9616b5cf9eebbda325329c2d58d
Indrek Ots <ots.indrek@gmail.com>
2018-06-11 18:35:25 +0300

    Capitalize constants

--- white-border.py
+++ white-border.py
@@ -2,8 +2,8 @@
 from PIL import Image
 import sys, os
 
-borderRatio = 11.52
-whiteBorderFolder = 'whiteBorder'
+BORDER_RATIO = 11.52
+WHITE_BORDER_FOLDER = 'whiteBorder'
 
 def isJpeg(fileName):
     return '.jpg' in fileName or '.jpeg' in fileName
@@ -11,17 +11,17 @@ def isJpeg(fileName):
 def addWhiteBorder(fileName):
     image = Image.open(fileName);
 
-    newSize = (image.size[0] + int(round(image.size[0] / borderRatio)),
-    image.size[1] + int(round(image.size[1] / borderRatio)))
+    newSize = (image.size[0] + int(round(image.size[0] / BORDER_RATIO)),
+    image.size[1] + int(round(image.size[1] / BORDER_RATIO)))
 
     print 'Adding a white border to ' + fileName
     newImage = Image.new("RGB", newSize, (255, 255, 255))
     newImage.paste(image, ((newImage.size[0] - image.size[0]) / 2, (newImage.size[1] - image.size[1]) / 2))
 
-    if not os.path.exists(whiteBorderFolder):
-        os.makedirs(whiteBorderFolder)
+    if not os.path.exists(WHITE_BORDER_FOLDER):
+        os.makedirs(WHITE_BORDER_FOLDER)
 
-    newImage.save(whiteBorderFolder + '/' + fileName)
+    newImage.save(WHITE_BORDER_FOLDER + '/' + fileName)
 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 for f in files:
